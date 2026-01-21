2861886d4c41da528c4a4996544cd0c4b063dc45
servecore <phpserve@outlook.com>
2026-01-21 12:03:47 +0700

    Fix missing dependencies: add install embla-carousel and embla-carousel-autoplay

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,18 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
 
+import accordion from './components/accordion';
+import avatar from './components/avatar';
+import carousel from './components/carousel';
+import checkbox from './components/checkbox';
+import collapsible from './components/collapsible';
+import radiogroup from './components/radio-group';
+
+Alpine.data('accordion', accordion);
+Alpine.data('avatar', avatar);
+Alpine.data('carousel', carousel);
+Alpine.data('checkbox', checkbox);
+Alpine.data('collapsible', collapsible);
+Alpine.data('radiogroup', radiogroup);
+
 Alpine.start();
